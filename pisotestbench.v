1f032d9ebeaaf78145e59d40d60e54c89952117d
VijaybhushanKoram <115135394+VijaybhushanKoram@users.noreply.github.com>
2022-10-06 21:02:10 +0530

    Add files via upload

---
+++ pisotestbench.v
@@ -0,0 +1,29 @@
+module piso_output;
+	reg [3:0] d;
+	reg clk;
+	reg mode;
+	wire so;
+	piso uut (
+		.d(d), 
+		.clk(clk), 
+		.mode(mode), 
+		.so(so)
+	); 
+	initial begin
+		forever begin
+		clk<=1;
+		#50;
+		clk<=0;
+		#50;
+		end
+	end 
+	initial begin 
+	mode=1;  
+	d=4'b1001;
+	#100;
+	mode=0; 
+	end
+	
+ 
+ endmodule
+ 
